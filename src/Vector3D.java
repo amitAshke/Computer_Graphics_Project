98ced882275b106885992e1bd4c6cdd03bcedbd1
Amit Ashkenazy <amit471@gmail.com>
2020-06-01 12:25:16 +0300

    Small fixes to the "Vector3D" class.

--- src/Vector3D.java
+++ src/Vector3D.java
@@ -1,6 +1,12 @@
 public class Vector3D {
     private double x, y, z;
 
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public final double getX() {
         return this.x;
     }
@@ -13,11 +19,11 @@ public class Vector3D {
         return this.z;
     }
 
-    public Vector3D(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
+    public void setX(double x) { this.x = x; }
+
+    public void setY(double y) { this.y = y; }
+
+    public void setZ(double z) { this.z = z; }
 
     public Vector3D clone() {
         return new Vector3D(x, y, z);
