584b097c6a816fd11eebdecf209b682b3ee5355a
Amit Ashkenazy <amit471@gmail.com>
2020-06-01 12:22:34 +0300

    Player now holds a "Controller" instead of an "InputHandler".

--- src/Player.java
+++ src/Player.java
@@ -3,45 +3,26 @@ import java.awt.event.KeyEvent;
 
 public class Player {
 
+    private Controller controller;
     private Camera camera;
-    private InputHandler inputHandler;
-    private double straightSpeed = 0.05;
-    private double diagonalSpeed = Math.sqrt(2) / 40;
 
-    public InputHandler getInputHandler() {
-        return inputHandler;
+    public Player() {
+
+        Vector3D position = new Vector3D(0.5, 1.2, 0.5), w_Vector = new Vector3D(0, 0, 1), v_Vector = new Vector3D(0, 1, 0);
+        camera = new Camera(position, w_Vector, v_Vector);
+        controller = new Controller();
     }
 
     public Camera getCamera() {
         return camera;
     }
 
-    public Player() {
-        inputHandler = new InputHandler();
-        camera = new Camera(new Vector3D(0.5, 1, 0.5),
-                new Vector3D(0, 0, 1),
-                new Vector3D(0, 1, 0));
-    }
+    public Controller getController() { return controller; }
 
     public void tick() {
-        if (inputHandler.key.get(KeyEvent.VK_W) && inputHandler.key.get(KeyEvent.VK_D)) {
-            camera.move_w(diagonalSpeed);
-            camera.move_u(diagonalSpeed);
-        } else if (inputHandler.key.get(KeyEvent.VK_W) && inputHandler.key.get(KeyEvent.VK_A)) {
-            camera.move_w(diagonalSpeed);
-            camera.move_u(-1 * diagonalSpeed);
-        } else if (inputHandler.key.get(KeyEvent.VK_S) && inputHandler.key.get(KeyEvent.VK_D)) {
-            camera.move_w(-1 * diagonalSpeed);
-            camera.move_u(diagonalSpeed);
-        } else if (inputHandler.key.get(KeyEvent.VK_S) && inputHandler.key.get(KeyEvent.VK_A)) {
-            camera.move_w(-1 * diagonalSpeed);
-            camera.move_u(-1 * diagonalSpeed);
-        } else {
-            if (inputHandler.key.get(KeyEvent.VK_W)) { camera.move_w(straightSpeed); }
-            if (inputHandler.key.get(KeyEvent.VK_S)) { camera.move_w(-1 * straightSpeed); }
-            if (inputHandler.key.get(KeyEvent.VK_D)) { camera.move_u(straightSpeed); }
-            if (inputHandler.key.get(KeyEvent.VK_A)) { camera.move_u(-1 * straightSpeed); }
-        }
+        controller.handleMovement(camera);
+
+        controller.handleRotation(camera);
     }
 
     public void setView(GLU glu) {
