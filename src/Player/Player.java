ed276714a4af72ff2e30fb58d77a9f0ccf1785a8
Amit Ashkenazy <amit471@gmail.com>
2020-06-09 11:29:10 +0300

    Created a "Player" package and moved the Player class to it.

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,14 +1,22 @@
+package Player;
+
+import LinearAlgebra.Vectors.Vector3D;
+
 import javax.media.opengl.glu.GLU;
-import java.awt.event.KeyEvent;
 
 public class Player {
 
     private Controller controller;
     private Camera camera;
 
+    public static final double HIT_RADIUS = 0.3;
+
     public Player() {
 
-        Vector3D position = new Vector3D(0.5, 1.2, 0.5), w_Vector = new Vector3D(0, 0, 1), v_Vector = new Vector3D(0, 1, 0);
+        Vector3D position = new Vector3D(1.3, 1.2, 3),
+//                w_Vector = new Vector3D(Math.sin(Math.toRadians(-30)), 0, Math.cos(Math.toRadians(-30))),
+                w_Vector = new Vector3D(0, 0, 1),
+                v_Vector = new Vector3D(0, 1, 0);
         camera = new Camera(position, w_Vector, v_Vector);
         controller = new Controller();
     }
@@ -20,6 +28,7 @@ public class Player {
     public Controller getController() { return controller; }
 
     public void tick() {
+
         controller.handleMovement(camera);
 
         controller.handleRotation(camera);
