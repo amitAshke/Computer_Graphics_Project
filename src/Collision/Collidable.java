284813e0996033edaa774648c4360ea053c81b52
Amit Ashkenazy <amit471@gmail.com>
2020-06-09 11:13:54 +0300

    Created a "Collision" package and created a "Collidable" interface.

---
+++ src/Collision/Collidable.java
@@ -0,0 +1,18 @@
+package Collision;
+
+import LinearAlgebra.Vectors.Vector3D;
+
+public interface Collidable {
+
+    public float getX1();
+
+    public float getX2();
+
+    public float getZ1();
+
+    public float getZ2();
+
+    public boolean isCollidingWithPlayer(Vector3D playerPosition);
+
+    public Vector3D handlePlayerCollision(Vector3D newPosition);
+}
