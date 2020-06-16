67d83647e340078cd4f0173e1134c6a2bbd26ea4
Amit Ashkenazy <amit471@gmail.com>
2020-06-16 14:19:43 +0300

    Small fix to player collision handling and added projectile collision handling.

--- src/Collision/CollisionHandler.java
+++ src/Collision/CollisionHandler.java
@@ -1,18 +1,27 @@
 package Collision;
 
 import LinearAlgebra.Vectors.Vector3D;
-import Main.World;
 
 import java.util.List;
 
 public class CollisionHandler {
-    public Vector3D handleCollisionWithPlayer(Vector3D newPosition) {
-        List<Collidable> collidables = World.playerCollidables;
+    public Vector3D handleCollisionWithPlayer(Vector3D newPosition, List<Collidable> collidables) {
         for (Collidable collidable : collidables) {
             if (collidable.isCollidingWithPlayer(newPosition)) {
                 newPosition = collidable.handlePlayerCollision(newPosition);
             }
         }
         return newPosition;
+    }
+
+    public Collidable handleCollisionWithProjectile(Vector3D position, double length, double radius, Vector3D direction, List<Collidable> collidables) {
+        Collidable collided = null;
+        for (Collidable collidable : collidables) {
+            if (collidable.isCollidingWithProjectile(position, length, radius, direction)) {
+                System.out.println("collision");
+                collided = collidable;
+            }
+        }
+        return collided;
     }
 }
