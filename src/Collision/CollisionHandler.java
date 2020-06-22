b707cb6f42c5e4b84bed7e64f93eb00813736c95
Amit Ashkenazy <amit471@gmail.com>
2020-06-22 15:38:28 +0300

    The "CollisionHandler" class now hold a "CollisionDetector" object and uses it.

--- src/Collision/CollisionHandler.java
+++ src/Collision/CollisionHandler.java
@@ -5,21 +5,44 @@ import LinearAlgebra.Vectors.Vector3D;
 import java.util.List;
 
 public class CollisionHandler {
-    public Vector3D handleCollisionWithPlayer(Vector3D newPosition, List<Collidable> collidables) {
+    private CollisionDetector detector;
+
+    public CollisionHandler() {
+        detector = new CollisionDetector();
+    }
+
+    public Vector3D handleCollisionWithPlayer(Capsule capsule, Vector3D newPosition, List<Collidable> collidables) {
         for (Collidable collidable : collidables) {
-            if (collidable.isCollidingWithPlayer(newPosition)) {
-                newPosition = collidable.handlePlayerCollision(newPosition);
+            Hitbox hitbox = collidable.getPlayerCollisionShape();
+            if (hitbox instanceof AABB) {
+                if(detector.detectCollision(capsule, (AABB) hitbox)) {
+                    newPosition = collidable.handlePlayerCollision(newPosition);
+                }
+            } else if (hitbox instanceof Capsule) {
+                if (detector.detectCollision(capsule, (Capsule) hitbox)) {
+//                    System.out.println("Before collision fix: " + newPosition);
+                    newPosition = collidable.handlePlayerCollision(newPosition);
+//                    System.out.println("After collision fix: " + newPosition + '\n');
+                }
             }
         }
         return newPosition;
     }
 
-    public Collidable handleCollisionWithProjectile(Vector3D position, double length, double radius, Vector3D direction, List<Collidable> collidables) {
+    public Collidable handleCollisionWithProjectile(Capsule capsule, List<Collidable> collidables) {
         Collidable collided = null;
         for (Collidable collidable : collidables) {
-            if (collidable.isCollidingWithProjectile(position, length, radius, direction)) {
-                System.out.println("collision");
-                collided = collidable;
+            Hitbox hitbox = collidable.getProjectileCollisionShape();
+            if (hitbox instanceof AABB) {
+                if(detector.detectCollision(capsule, (AABB) hitbox)) {
+//                    System.out.println("projectile collision with aabb");
+                    collided = collidable;
+                }
+            } else if (hitbox instanceof Capsule) {
+                if (detector.detectCollision(capsule, (Capsule) hitbox)) {
+//                    System.out.println("projectile collision + with capsule");
+                    collided = collidable;
+                }
             }
         }
         return collided;
