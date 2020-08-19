326772f650775a3a15012c6132d682969a494e4c
Amit Ashkenazy <amit471@gmail.com>
2020-08-19 12:09:49 +0300

    Organized the "Controller" class code.

--- src/Player/Controller.java
+++ src/Player/Controller.java
@@ -35,7 +35,7 @@ public class Controller {
         Camera camera = Player.camera;
         HashMap<Integer, Boolean> key = inputHandler.key;
 
-        Vector3D oldPosition = camera.position, newPosition = oldPosition, u_Projection = camera.u_Vector, w_Projection;
+        Vector3D newPosition = camera.position, u_Projection = camera.u_Vector, w_Projection;
 
         // Make projection of the forward vector of the camera to the XZ plane.
         if (camera.v_Vector.getY() > 0) {
@@ -50,44 +50,67 @@ public class Controller {
 
         // If more than one movement button is pressed.
         if (inputHandler.getMovementKeysPressed() > 1) {
-            if (key.get(KeyEvent.VK_W) && key.get(KeyEvent.VK_D)) {
-                newPosition = newPosition.scaleAdd(diagonalSpeed, w_Projection);
-                newPosition = newPosition.scaleAdd(diagonalSpeed, u_Projection);
-
-            }
-            if (key.get(KeyEvent.VK_W) && key.get(KeyEvent.VK_A)) {
-                newPosition = newPosition.scaleAdd(diagonalSpeed, w_Projection);
-                newPosition = newPosition.scaleAdd(-1 * diagonalSpeed, u_Projection);
-
-            }
-            if (key.get(KeyEvent.VK_S) && key.get(KeyEvent.VK_D)) {
-                newPosition = newPosition.scaleAdd(-1 * diagonalSpeed, w_Projection);
-                newPosition = newPosition.scaleAdd(diagonalSpeed, u_Projection);
-
-            }
-            if (key.get(KeyEvent.VK_S) && key.get(KeyEvent.VK_A)) {
-                newPosition = newPosition.scaleAdd(-1 * diagonalSpeed, w_Projection);
-                newPosition = newPosition.scaleAdd(-1 * diagonalSpeed, u_Projection);
-            }
+            newPosition = handleDiagonalMovement(newPosition, w_Projection);
         }
         // If a single movement button is pressed.
         else {
-            if (key.get(KeyEvent.VK_W)) {
-                newPosition = newPosition.scaleAdd(straightSpeed, w_Projection);
-            }
-            if (key.get(KeyEvent.VK_S)) {
-                newPosition = newPosition.scaleAdd(-1 * straightSpeed, w_Projection);
-            }
-            if (key.get(KeyEvent.VK_D)) {
-                newPosition = newPosition.scaleAdd(straightSpeed, u_Projection);
-            }
-            if (key.get(KeyEvent.VK_A)) {
-                newPosition = newPosition.scaleAdd(-1 * straightSpeed, u_Projection);
-            }
+            newPosition = handleStraightMovement(newPosition, w_Projection);
         }
 
         // Sets the new position of the camera.
         camera.position = newPosition;
+    }
+
+    /**
+     * This functions determines the diagonal direction according to the player's input and returns the new player location.
+     */
+    private Vector3D handleDiagonalMovement(Vector3D newPosition, Vector3D w_Projection) {
+        HashMap<Integer, Boolean> key = inputHandler.key;
+        Vector3D u_Projection = Player.camera.u_Vector;
+
+        if (key.get(KeyEvent.VK_W) && key.get(KeyEvent.VK_D)) {
+            newPosition = newPosition.scaleAdd(diagonalSpeed, w_Projection);
+            newPosition = newPosition.scaleAdd(diagonalSpeed, u_Projection);
+
+        }
+        if (key.get(KeyEvent.VK_W) && key.get(KeyEvent.VK_A)) {
+            newPosition = newPosition.scaleAdd(diagonalSpeed, w_Projection);
+            newPosition = newPosition.scaleAdd(-1 * diagonalSpeed, u_Projection);
+
+        }
+        if (key.get(KeyEvent.VK_S) && key.get(KeyEvent.VK_D)) {
+            newPosition = newPosition.scaleAdd(-1 * diagonalSpeed, w_Projection);
+            newPosition = newPosition.scaleAdd(diagonalSpeed, u_Projection);
+
+        }
+        if (key.get(KeyEvent.VK_S) && key.get(KeyEvent.VK_A)) {
+            newPosition = newPosition.scaleAdd(-1 * diagonalSpeed, w_Projection);
+            newPosition = newPosition.scaleAdd(-1 * diagonalSpeed, u_Projection);
+        }
+
+        return newPosition;
+    }
+
+    /**
+     * This functions determines the straight direction according to the player's input and returns the new player location.
+     */
+    private Vector3D handleStraightMovement(Vector3D newPosition, Vector3D w_Projection) {
+        HashMap<Integer, Boolean> key = inputHandler.key;
+        Vector3D u_Projection = Player.camera.u_Vector;
+
+        if (key.get(KeyEvent.VK_W)) {
+            newPosition = newPosition.scaleAdd(straightSpeed, w_Projection);
+        }
+        if (key.get(KeyEvent.VK_S)) {
+            newPosition = newPosition.scaleAdd(-1 * straightSpeed, w_Projection);
+        }
+        if (key.get(KeyEvent.VK_D)) {
+            newPosition = newPosition.scaleAdd(straightSpeed, u_Projection);
+        }
+        if (key.get(KeyEvent.VK_A)) {
+            newPosition = newPosition.scaleAdd(-1 * straightSpeed, u_Projection);
+        }
+        return newPosition;
     }
 
     /**
@@ -114,41 +137,53 @@ public class Controller {
 
         // If the player clicked on the left mouse button.
         if (inputHandler.key.get(1)) {
-            // If the player has not reached his projectile limit with or without the special attack.
-            if ((!altActive && Player.projectiles.size() < Player.projectileLimit) ||
-                    (altActive && Player.projectiles.size() + Player.altProjectile.getProjectiles().size() < Player.projectileLimit)) {
-                long currentTimeFire = System.nanoTime(), passedTime = currentTimeFire - lastTimeFired;
-                // If the time that passed since the last shot is greater than the cooldown.
-                if (passedTime / 1000000000.0 > Player.COOLDOWN) {
-                    lastTimeFired = currentTimeFire;
-
-                    Player.projectiles.add(new StandardProjectile(camera.position, camera.w_Vector, camera.v_Vector, -1));
-                    soundPlayer.playStandard();
-                }
-            }
-            inputHandler.resetMouseButtons();
+            handleStandardProjectileActivation();
         }
         // If the player clicked on the right button.
         else if (inputHandler.key.get(3)) {
-            if (!altActive && Player.projectiles.size() == 0) {
-                Player.altProjectile = new AltProjectile(camera.position, new Vector3D(0, 0, 1));
-                inputHandler.resetMouseButtons();
-                altActive = true;
-                soundPlayer.playSpecial();
-            } else if (altActive) {
-                Player.altProjectile.detachProjectiles();
-                Player.projectiles = Player.altProjectile.getProjectiles();
-                altActive = false;
-                inputHandler.resetMouseButtons();
-                Player.altProjectile = null;
-            }
+            handleSpecialProjectileActivation();
         }
 
         // Reset the mouse buttons.
         inputHandler.resetMouseButtons();
     }
 
-    public void resetValues() {
+    private void handleStandardProjectileActivation() {
+        Camera camera = Player.camera;
+
+        // If the player has not reached his projectile limit with or without the special attack.
+        if ((!altActive && Player.projectiles.size() < Player.projectileLimit) ||
+                (altActive && Player.projectiles.size() + Player.altProjectile.getProjectiles().size() < Player.projectileLimit)) {
+            long currentTimeFire = System.nanoTime(), passedTime = currentTimeFire - lastTimeFired;
+            // If the time that passed since the last shot is greater than the cooldown.
+            if (passedTime / 1000000000.0 > Player.COOLDOWN) {
+                lastTimeFired = currentTimeFire;
+
+                Player.projectiles.add(new StandardProjectile(camera.position, camera.w_Vector, camera.v_Vector, -1));
+                soundPlayer.playStandard();
+            }
+        }
+        inputHandler.resetMouseButtons();
+    }
+
+    private void handleSpecialProjectileActivation() {
+        Camera camera = Player.camera;
+
+        if (!altActive && Player.projectiles.size() == 0) {
+            Player.altProjectile = new AltProjectile(camera.position, new Vector3D(0, 0, 1));
+            inputHandler.resetMouseButtons();
+            altActive = true;
+            soundPlayer.playSpecial();
+        } else if (altActive) {
+            Player.altProjectile.detachProjectiles();
+            Player.projectiles = Player.altProjectile.getProjectiles();
+            altActive = false;
+            inputHandler.resetMouseButtons();
+            Player.altProjectile = null;
+        }
+    }
+
+        public void resetValues() {
         altActive = false;
     }
 }
